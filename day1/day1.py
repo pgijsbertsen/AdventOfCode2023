6882650f4b46d80eb0d1cffb128b42af5d07aeb9
pgijsbertsen <117165507+pgijsbertsen@users.noreply.github.com>
2023-12-01 20:07:27 +0100

    Day 1

---
+++ day1/day1.py
@@ -0,0 +1,31 @@
+# Read input
+input = open("./input.txt", "r")
+
+# Do stuff
+def sum_digits():
+    sum_input = []
+    for line in input:
+        stripped_line = ''.join(i for i in line if i.isdigit())
+        first_digit = stripped_line[0]
+        last_digit = stripped_line[-1]
+        digits = first_digit + last_digit
+        sum_input.append(int(digits))
+    return sum(sum_input)
+
+def sum_chars():
+    sum_input = []
+    digits = []
+    numbers = {'one' : 'o1ne', 'two' : 't2wo', 'three' : 'th3ree', 'four' : 'fo4ur', 'five' : 'fi5ve', 'six' : 's6ix', 'seven' : 'se7ven', 'eight' : 'eig8ht', 'nine' : 'ni9ne'}
+    for line in input:
+        for word, number in numbers.items():
+            line = line.replace(word, number)
+        stripped_line = ''.join(i for i in line if i.isdigit())
+        first_digit = stripped_line[0]
+        last_digit = stripped_line[-1]
+        digits = first_digit + last_digit
+        sum_input.append(int(digits))
+    return sum(sum_input)
+
+# Print result
+result = sum_chars()
+print('Result: ', result)
