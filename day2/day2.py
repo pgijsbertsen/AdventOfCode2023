f916b82892c55d70369145bcca36b5816b515925
pgijsbertsen <117165507+pgijsbertsen@users.noreply.github.com>
2023-12-02 23:34:56 +0100

    Day2 puzzle2

--- day2/day2.py
+++ day2/day2.py
@@ -1,8 +1,5 @@
-import re
-
-
 # Read input
-input = open("inputs/input.txt", "r")
+input = open("inputs/input.txt", "r").read().split('\n')
 
 max_cubes = {
     'red' : 12,
@@ -10,6 +7,7 @@ max_cubes = {
     'blue' : 14
     }
 
+# Do stuff
 def puzzle1():
     idsum = 0
     for line in input:
@@ -28,5 +26,34 @@ def puzzle1():
             idsum += gamenum
     return(idsum)
 
-result = puzzle1()
-print('Result: ', result)
+def puzzle2(row):
+    rMax = 0
+    gMax = 0
+    bMax = 0
+    game = row.split(":")
+    gamenum = int(game[0].split()[1])
+    draws = game[1].split(";")
+    for draw in draws:
+        cubes = draw.split(',')
+        for cube in cubes:
+            rgb = cube.split(' ')
+            num = int(rgb[1])
+            color = rgb[2]
+            if color == "red":
+                if num > rMax:
+                    rMax = num
+            if color == "green":
+                if num > gMax:
+                    gMax = num
+            if color == "blue":
+                if num > bMax:
+                    bMax = num
+    power = rMax * gMax * bMax
+    return power
+
+# Gather and print results
+result1 = puzzle1()
+result2 = 0
+for line in input:
+    result2 += puzzle2(line)
+print('Result 1:', result1, '\nResult 2:', result2)
